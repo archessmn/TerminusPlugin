e72f8b0b2bbd7b56fe20a03dfe0d4f3441b430d6
archessmn <63592337+archessmn@users.noreply.github.com>
2020-11-24 11:38:48 +0000

    Add files via upload

---
+++ main.java
@@ -0,0 +1,19 @@
+package com.github.archessmn.TerminusPlugin;
+
+import com.github.archessmn.TerminusPlugin.commands.spawn;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin {
+
+    public void onEnable() {
+        PluginManager pm = this.getServer().getPluginManager();
+
+
+        this.getCommand("spawn").setExecutor(new spawn());
+    }
+
+    public void onDisable() {
+
+    }
+}
