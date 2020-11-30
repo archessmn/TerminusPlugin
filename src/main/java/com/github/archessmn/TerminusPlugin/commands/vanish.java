7ee640c4b8062b9de8caa60effb61018e727e9b6
archessmn <max.moir@icloud.com>
2020-11-30 08:15:36 +0000

    Testing permissions

--- src/main/java/com/github/archessmn/TerminusPlugin/commands/vanish.java
+++ src/main/java/com/github/archessmn/TerminusPlugin/commands/vanish.java
@@ -1,30 +1,23 @@
 package com.github.archessmn.TerminusPlugin.commands;
 
-import com.github.archessmn.TerminusPlugin.interfaces.permsPlayer;
-import com.github.archessmn.TerminusPlugin.ymlFiles.playerData;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
-import java.util.Objects;
 
 public class vanish implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command cmd, String label, String[] args) {
         Player player = (Player) commandSender;
 
-        if (hasCustomPermission("terminus.commands.vanish", player)) {
+        if (player.hasPermission("terminus.commands.vanish")) {
             player.setInvisible(true);
             player.setCustomNameVisible(true);
+        } else {
+            player.sendMessage("§7[§4§lE§7] You don't have the permissions for this: terminus.commands.vanish");
         }
 
 
         return true;
-    }
-
-
-    public boolean hasCustomPermission(String permission, Player player) {
-        return Objects.requireNonNull(playerData.get().getList(player.getUniqueId() + ".permissions")).contains(permission);
     }
 }
